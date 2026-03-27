f7817f0275e3ed378951dd364e50bf9c8d082b86
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-03-27 12:47:50 +0100

    Test de Pull Request

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
       <p>✔ git commit : Sauvegarder</p>
       <p>✔ git diff : Voir les modiffications de l'espace de travail par raport à la dernière version enregistée dans le repos</p>
       <p>✔ git diff --cached : Voir les modiffications des fichier indexé (git add)</p>
+      <p>✔ git checkout -b dev : créer et basculer sur une nouvelle branche</p>
     </>
   )
 }
