faae213aef74851be293ade1fa75a189f5c8a62b
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-15 23:19:37 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ function App() {
       <p>✔ git commit : Sauvegarder</p>
       <p>✔ git diff : Voir les modiffications de l'espace de travail par raport à la dernière version enregistée dans le repos</p>
       <p>✔ git diff --cached : Voir les modiffications des fichier indexé (git add)</p>
+      <p>✔ git checkout -b [branch] : Change de branche et ce placer sur le dernier commit de celle-ci. -b pour en plus créer la branche.</p>
+      <p>✔ git rebase [branch] : met à jour votre branche avec les derniers commits de [branch]</p>
       
     </>
   )
