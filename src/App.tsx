7db142f757fdec091a6a28a8637af1d9cb847165
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-15 23:19:28 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ function App() {
       <p>✔ git reset : Retirer indexation</p>
       <p>✔ git reset HEAD~2: Annuler les deux dernière commit</p>
       <p>✔ git commit : Sauvegarder</p>
+      <p>✔ git diff : Voir les modiffications de l'espace de travail par raport à la dernière version enregistée dans le repos</p>
+      <p>✔ git diff --cached : Voir les modiffications des fichier indexé (git add)</p>
       
     </>
   )
