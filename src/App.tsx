510e242e89a78aef3268ba4cca8dcb1d0f9ff14f
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-22 22:44:33 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ function App() {
       <p>✔ git init : Initialiser un dépôt (repository) local.</p>
       <p>✔ git clone [url] : Cloner un projet existant depuis un dépôt distant.</p>
       <p>✔ git status : Voir l'état actuel des fichiers (suivis, modifiés, indexés).</p>
+      <p>✔ git add [fichier] : Indexer un fichier (Staging Area). Utiliser `git add .` pour tout indexer.</p>
+      <p>✔ git reset [fichier] : Retirer un fichier de l'indexation sans annuler les modifications.</p>
       
     </>
   )
