39c06906fef4f0b024e3f56e9c146b94c9e2a87b
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-22 22:44:04 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
       <h1>Cours GITHUB</h1>
       <p>Apprentissage...</p>
 
+      {/* ================= SECTION GIT ================= */}
+      <h2>GIT (Local)</h2>
+      <p>✔ git init : Initialiser un dépôt (repository) local.</p>
+      <p>✔ git clone [url] : Cloner un projet existant depuis un dépôt distant.</p>
+      <p>✔ git status : Voir l'état actuel des fichiers (suivis, modifiés, indexés).</p>
+      
     </>
   )
 }
